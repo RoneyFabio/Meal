f67327d940546fa56882f22648928fa93ff1dda3
RoneyFabio <roneyfabio@gmail.com>
2023-05-26 12:50:01 -0300

    182

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'screens/categories_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Vamos Cozinhar?',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Vamos Cozinhar?'),
-      ),
-      body: Center(
-        child: Text('Navegar é preciso!!'),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
